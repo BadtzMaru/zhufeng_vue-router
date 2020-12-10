d5e46b1a25fc28fcee89bebdefe4a535f41e9d99
BadtzMaru <707380414@qq.com>
2020-12-10 22:30:01 +0800

    2020-12-10 22:29

---
+++ src/vue-router/createRouteMap.js
@@ -0,0 +1,30 @@
+function createRouteMap(routes, oldPathMap) {
+	let pathMap = oldPathMap || Object.create(null); // 没有原型链
+
+	routes.forEach((route) => {
+		addRouteRecord(route, pathMap);
+	});
+
+	return {
+		pathMap,
+	};
+}
+
+function addRouteRecord(route, pathMap, parent) {
+	let path = parent ? `${parent.path}/${route.path}` : route.path;
+	let record = {
+		path,
+		name: route.name,
+		component: route.component,
+	};
+	// 根据路径匹配记录
+	pathMap[path] = record;
+	// 如果有儿子就递归
+	if (route.children) {
+		route.children.forEach((r) => {
+			addRouteRecord(r, pathMap, record);
+		});
+	}
+}
+
+export default createRouteMap;
---
+++ src/vue-router/history/html5.js
@@ -0,0 +1,17 @@
+import Base from './base';
+class Html5 extends Base {
+	constructor(router) {
+		super(router);
+	}
+	setupListener() {
+		return window.location.pathname;
+	}
+	getCurrentLocation() {
+		// 监控浏览器 前进后退变化
+		window.addEventListener('popstate', () => {
+			this.transitionTo(this.getCurrentLocation());
+		});
+	}
+}
+
+export default Html5;
